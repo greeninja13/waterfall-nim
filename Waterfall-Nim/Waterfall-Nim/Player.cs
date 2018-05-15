b8b4a031fb3dade577d8f61285f61f26f4333cd2
Destiny Zinke <dzinke@student.neumont.edu>
2018-05-15 00:57:49 -0600

    Names Added
    Players are able to name themselves

--- Waterfall-Nim/Waterfall-Nim/Player.cs
+++ Waterfall-Nim/Waterfall-Nim/Player.cs
@@ -24,6 +24,29 @@ namespace Waterfall_Nim
             set { p2Name = value; }
         }
 
+        public void Name()
+        {
+            Console.WriteLine("Player 1, what is your name?");
+            Console.WriteLine();
+            P1Name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Player 2, what is your name?>");
+            Console.WriteLine();
+            P2Name = Console.ReadLine();
+            Turn();
+        }
 
+        public string Turn()
+        {
+            string playerTurn = "p1";
+            Random rand = new Random();
+            int num = rand.Next(1, 2);
+            if (num == 2)
+            {
+                playerTurn = "p2";
+            }
+
+            return playerTurn;
+        }
     }
 }
