4c3412e4aefd53bf7178164124b656f88b644547
Brad Gustafson <greeninja13@gmail.com>
2018-05-14 18:46:17 -0600

    Added crap

---
+++ Waterfall-Nim/Waterfall-Nim/models/Board.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterfall_Nim.models
+{
+    public class Board
+    {
+        private Heap[] heaps;
+        public Board(int difficulty)
+        {
+            switch (difficulty)
+            {
+                case 1: //Easy
+                    heaps = new Heap[] { new Heap() { Sticks = 3 }, new Heap() { Sticks = 3 } };
+                    break;
+                case 2: //Medium
+                    heaps = new Heap[] { new Heap() { Sticks = 2 }, new Heap() { Sticks = 5 }, new Heap() { Sticks = 7 } };
+                    break;
+                case 3: //Hard
+                    heaps = new Heap[] { new Heap() { Sticks = 2 }, new Heap() { Sticks = 3 }, new Heap() { Sticks = 8 }, new Heap() { Sticks = 9 } };
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Waterfall-Nim/Waterfall-Nim/models/Heap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterfall_Nim.models
+{
+    class Heap
+    {
+        public int Sticks { get; set; }
+
+        public void Remove(int sticks)
+        {
+            Sticks -= sticks;
+        }
+
+        public bool isEmpty()
+        {
+           return (Sticks == 0) ? true : false;
+        }
+    }
+}
