a418d3854e10ba668986d95ab72fe8dead1445b4
Destiny Zinke <dzinke@student.neumont.edu>
2018-05-14 23:17:22 -0600

    Menu Added
    Gives choices to start game/do instructions

--- Waterfall-Nim/Waterfall-Nim/Program.cs
+++ Waterfall-Nim/Waterfall-Nim/Program.cs
@@ -10,6 +10,25 @@ namespace Waterfall_Nim
     {
          public static void Main(string[] args)
         {
+            
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine();
+            Console.WriteLine("1. Play Game");
+            Console.WriteLine("2. Read instructions");
+            string input = Console.ReadLine();
+            input = input.ToUpper();
+            if (input.Contains("1") || input.Contains("play"))
+            {
+                // start game
+            }
+            else if (input.Contains("2") || input.Contains("read"))
+            {
+                Instructions();
+            }
         }
 
         public static void Instructions()
@@ -17,6 +36,19 @@ namespace Waterfall_Nim
             Console.WriteLine("1. You may remove any number of sticks from any heap");
             Console.WriteLine();
             Console.WriteLine("2. If you are the one to remove the last stick, you lose");
+            Console.WriteLine();
+            Console.WriteLine("Press B to go back");
+            Console.WriteLine();
+            string input = Console.ReadLine();
+            input = input.ToUpper();
+            if(input == "B")
+            {
+                Menu();
+            }
+            else
+            {
+            }
+
         }
     }
 }
