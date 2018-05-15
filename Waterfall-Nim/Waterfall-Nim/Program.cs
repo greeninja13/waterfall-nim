336f0c11391d11a7c69f066867cdb28eadff0638
Destiny Zinke <dzinke@student.neumont.edu>
2018-05-14 23:40:49 -0600

    Validation Added
    Added validation for start menu & instructions

--- Waterfall-Nim/Waterfall-Nim/Program.cs
+++ Waterfall-Nim/Waterfall-Nim/Program.cs
@@ -10,45 +10,66 @@ namespace Waterfall_Nim
     {
          public static void Main(string[] args)
         {
-            
+            Start();   
         }
 
-        public static void Menu()
+        public static void Start()
         {
-            Console.WriteLine("What would you like to do?");
-            Console.WriteLine();
-            Console.WriteLine("1. Play Game");
-            Console.WriteLine("2. Read instructions");
-            string input = Console.ReadLine();
-            input = input.ToUpper();
-            if (input.Contains("1") || input.Contains("play"))
+            bool valid = false;
+            do
             {
-                // start game
-            }
-            else if (input.Contains("2") || input.Contains("read"))
-            {
-                Instructions();
-            }
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine();
+                Console.WriteLine("1. Play Game");
+                Console.WriteLine("2. Read instructions");
+                string input = Console.ReadLine();
+                Console.WriteLine();
+                input = input.ToUpper();
+                if (input.Contains("1") || input.Contains("play"))
+                {
+                    // start game
+                    valid = true;
+                }
+                else if (input.Contains("2") || input.Contains("read"))
+                {
+                    Instructions();
+                }else if(input == "" || input == null)
+                {
+                    Console.WriteLine("Input was invalid");
+                }
+                else
+                {
+                    Console.WriteLine("Input was invalid");
+                }
+            } while (!valid);
         }
 
         public static void Instructions()
         {
-            Console.WriteLine("1. You may remove any number of sticks from any heap");
-            Console.WriteLine();
-            Console.WriteLine("2. If you are the one to remove the last stick, you lose");
-            Console.WriteLine();
-            Console.WriteLine("Press B to go back");
-            Console.WriteLine();
-            string input = Console.ReadLine();
-            input = input.ToUpper();
-            if(input == "B")
+            bool valid = false;
+            do
             {
-                Menu();
-            }
-            else
-            {
-            }
-
+                Console.WriteLine("1. You may remove any number of sticks from any heap");
+                Console.WriteLine();
+                Console.WriteLine("2. If you are the one to remove the last stick, you lose");
+                Console.WriteLine();
+                Console.WriteLine("Press B to go back");
+                Console.WriteLine();
+                string input = Console.ReadLine();
+                input = input.ToUpper();
+                if (input == "B")
+                {
+                    Start();
+                }
+                else if (input == "" || input == null)
+                {
+                    Console.WriteLine("Input was invalid");
+                }
+                else
+                {
+                    Console.WriteLine("Input was invalid");
+                }
+            } while (!valid);
         }
     }
 }
